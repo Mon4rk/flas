59b4931406ea0771a833b7256373d67dd17551e1
alisson <alisson.campos.almeida@escola.pr.gov.br>
2025-05-21 11:49:41 -0300

    3

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-
     cartao.innerHTML = `
     <div class="cartao__conteudo">
     <h3>${categoria}</h3>
